a2799b6f73211b4543debd8549dacad9076e357b
bmstyojaop <bmstyojaop@gmail.com>
2020-08-06 17:23:21 +0900

    add associations and validates

--- app/models/sending_destination.rb
+++ app/models/sending_destination.rb
@@ -1,2 +1,4 @@
 class SendingDestination < ApplicationRecord
+  belongs_to :user, optional: true
+  validates :post_code, :prefecture_code, :city, :house_number, :destination_family_name, :destination_first_name, :destination_family_name_kana, :destination_first_name_kana, presence: true
 end
