f14d3c14f0cdacb0e58aa3bb979d4f8d7c7ee298
bmstyojaop <bmstyojaop@gmail.com>
2020-08-07 13:37:19 +0900

    delete prefix from date_select

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2020_08_06_072821) do
     t.string "first_name", null: false
     t.string "family_name_kana", null: false
     t.string "first_name_kana", null: false
-    t.integer "birthday", null: false
+    t.date "birthday", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
