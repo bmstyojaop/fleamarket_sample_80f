9915e133045f3885a8e65838626bf841b67c009c
keita73 <keitasakuma73@icloud.com>
2020-08-10 17:55:17 +0900

    itemモデルアソシエーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,27 @@ class Item < ApplicationRecord
   belongs_to_active_hash :item_condition
   # belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id'
   # belongs_to :seller, class_name: 'User', foreign_key: 'seller_id'
+
+
+  belongs_to_active_hash :categories
+  belongs_to_active_hash :Shipping
+
+  has_many :images
+ 
+  validates :item_name,                    presence: true,    length: { maximum: 40 } 
+  validates :item_introduction,            presence: true,    length: { maximum: 1000 } 
+  validates :category_id,                  presence: true
+  validates :item_condition,               presence: true
+  validates :auction_status,               presence: true
+  validates :postage_payer,                presence: true
+  validates :shipping_origin,              presence: true
+  validates :preparation_day,              presence: true
+  validates :price,                        presence: true
+  validates :images,                       presence: true
+
+  validates_associated :images
+
+  enum auction_status: {出品中: 1, 売り切れ: 2}
+  accepts_nested_attributes_for :images, allow_destroy: true
+
 end
