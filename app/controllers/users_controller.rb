af48427db028845f976752f84dbc4499b8c298d7
bmstyojaop <bmstyojaop@gmail.com>
2020-08-10 18:26:37 +0900

    cerate profile_table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
   def show
+    @nickname = current_user.nickname
+    @address = current_user.sending_destination.address_street
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-ActiveRecord::Schema.define(version: 2020_08_06_091952) do
+ActiveRecord::Schema.define(version: 2020_08_10_090116) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -80,6 +78,13 @@ ActiveRecord::Schema.define(version: 2020_08_06_091952) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "image"
+    t.text "introduction"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "sending_destinations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "phone_number"
     t.integer "post_code", null: false
@@ -96,28 +101,6 @@ ActiveRecord::Schema.define(version: 2020_08_06_091952) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_sending_destinations_on_user_id"
   end
-
-ActiveRecord::Schema.define(version: 2020_08_06_225227) do
-
-  create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "item_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "item_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "item_id", null: false
-    t.string "url", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-
-ActiveRecord::Schema.define(version: 2020_08_06_033715) do
-
-
-
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
