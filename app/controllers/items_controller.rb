c196e0761eb812a8cb98794c1f6ec98411cb9007
keita73 <keitasakuma73@icloud.com>
2020-08-10 19:43:04 +0900

    items_controller追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,26 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+    @item.images.new
+  end
+
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to  items_path
+    else
+      @item.images.new
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :item_introduction, :item_condition, :auction_status, :postage_payer, :shipping_origin, :price,:author, :company, :reparation_day, :category_id, :shipping_origin, :postage_payer, :images_attributes [:image, :id])
+  end
+  
 end
