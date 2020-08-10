93f2de263fd5d1c56cf4ed192fb1d639621a986d
keita73 <keitasakuma73@icloud.com>
2020-08-10 18:01:43 +0900

    PostageType追記

--- app/models/postage_type.rb
+++ app/models/postage_type.rb
@@ -1,3 +1,12 @@
 class PostageType < ActiveHash::Base
   # has_many :items
+
+  self.data = [
+    {id: 1, name: 'ヤマト宅急便'}, {id: 2, name: 'ゆうパック'}, {id: 3, name: 'ヤマト便'},
+    {id: 4, name: '飛脚宅配便'}
+  ]
+
+  include ActiveHash::Associations
+  has_many :items
+
 end
