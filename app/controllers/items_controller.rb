2b301b0b28cd3c0eb393368d0761531b99bf5a85
keita73 <keitasakuma73@icloud.com>
2020-08-14 21:26:15 +0900

    items_controller修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
 
+  before_action :set_item, only: [:show, :destroy]
+
   def index
     @items = Item.includes(:images).order('created_at DESC')
   end
@@ -21,14 +23,12 @@ class ItemsController < ApplicationController
 
 
   def show
-    @item = Item.find(params[:id])
     @user = User.find(@item.user_id)
     @images = Image.where(item_id: params[:id])
     @images_first = Image.where(item_id: params[:id]).first
   end
 
   def destroy
-    @item = Item.find(params[:id])
     if @item.destroy
       redirect_to  root_path
     else
@@ -41,6 +41,8 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :item_introduction, :item_condition_id, :postage_payer_id, :price,:author, :company, :preparation_day_id, :category_id, :shipping_origin_id, :postage_type_id, images_attributes: [:image, :id]).merge(user_id: current_user.id)
   end
-  
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
