5d2a31ec108212d3b40a1b8a21709d3ce1cbf9d6
bmstyojaop <bmstyojaop@gmail.com>
2020-08-15 15:32:33 +0900

    delet migration (addclumn user_id)

--- db/schema.rb
+++ db/schema.rb
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 2020_08_13_084424) do
     t.integer "auction_status", default: 1, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
