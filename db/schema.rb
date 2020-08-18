4f6568bfe6c6d5467f0b0604901073cbceade3a4
keita73 <keitasakuma73@icloud.com>
2020-08-18 15:16:24 +0900

    schema.rb修正

--- db/schema.rb
+++ db/schema.rb
@@ -80,7 +80,7 @@ ActiveRecord::Schema.define(version: 2020_08_18_015107) do
   create_table "sending_destinations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "phone_number"
     t.integer "post_code", null: false
-    t.integer "prefecture_code", null: false
+    t.string "prefecture_code", null: false
     t.string "address_city", null: false
     t.string "address_street", null: false
     t.string "address_building"
@@ -101,6 +101,9 @@ ActiveRecord::Schema.define(version: 2020_08_18_015107) do
     t.string "family_name_kana", null: false
     t.string "first_name_kana", null: false
     t.date "birthday", null: false
+    t.text "background"
+    t.text "image"
+    t.text "introduction"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
