72c117a0036c40a2adcd56a9be37db738d505193
keita73 <keitasakuma73@icloud.com>
2020-08-24 18:20:39 +0900

    favorites.haml,scss作成

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   root to: "items#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :edit, :update] do
+    collection do
+      get 'favorites'
+    end
     resources :sending_destinations, only: [:edit, :update]
     member do 
       get :item_list
