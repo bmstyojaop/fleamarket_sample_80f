11efa1adbdd6368e55aa55feec601f0c40ae2777
bmstyojaop <bmstyojaop@gmail.com>
2020-08-27 12:10:04 +0900

    link for category_items

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,18 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: :show
+
+  def show
+    @items = @category.set_categories.order("created_at DESC").page(params[:page]).per(9)
+    # @items = @items.where(user_id: nil).order("created_at DESC").page(params[:page]).per(9)
+  end
+
+  private
+  def set_category
+    @category = Category.find(params[:id])
+    if @category.has_children?
+      @category_links = @category.children
+    else
+      @category_links = @category.siblings
+    end
+  end
+end
